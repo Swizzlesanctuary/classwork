288e609d5ca046546b5d7a63152fd5647d0466b3
Swizzlesanctuary <gvantsajishkariani0@gmail.com>
2025-11-01 12:39:15 +0100

    DOM Selector homework 1

--- main.js
+++ main.js
@@ -1,34 +1,5 @@
-let zoomInButton = document.getElementById('button1');
-let zoomOutButton = document.getElementById('button2');
-let background = document.getElementById('background');
-let hideButton = document.getElementById('hide');
-let showButton = document.getElementById('show');
-let colorButton = document.getElementById('colorButton');
-let textColor = document.getElementById('textColor');
 
-zoomInButton.addEventListener('click', () => {
-  background.classList.add('fullscreen');
-});
-
-zoomOutButton.addEventListener('click', () => {
-  background.classList.remove('fullscreen');
-});
-
-showButton.addEventListener('click',()=>{
-  background.classList.remove('hide');
-})
-
-hideButton.addEventListener('click',()=>{
-  background.classList.add('hide');
-})
-
-
-let colors = ['pink', 'green', 'yellow'];
-let clickCount = 0;
-
-colorButton.addEventListener('click', () => {
-  const currentColor = colors[clickCount % colors.length];
-  textColor.style.color = currentColor;
-  textColor.textContent = currentColor;
-  clickCount++;
-});
+document.querySelector("h1").style.Color = "blue";
+document.querySelector("h2").style.backgroundColor = "blue";
+document.querySelector("h2").style.color = "white";
+document.querySelector("span").style.fontSize = "200%";
